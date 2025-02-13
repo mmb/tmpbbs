cbc3a44147bee198478c916a86fbf8c21576cd14
Matthew Boedicker <24275+mmb@users.noreply.github.com>
2025-02-12 22:42:32 -0800

    Change TAG to VERSION in build script

--- build-release-binaries.sh
+++ build-release-binaries.sh
@@ -2,33 +2,33 @@
 
 set -eu
 
-TAG=$1
+VERSION=$1
 COMMIT=$2
 
 export RELEASE_DIR=release
 mkdir -p "$RELEASE_DIR"
 
-BUILD_ARGS=(-ldflags "-s -w -X main.version=$TAG-$COMMIT")
+BUILD_ARGS=(-ldflags "-s -w -X main.version=$VERSION-$COMMIT")
 
 export CGO_ENABLED=0
 
 export GOOS=darwin
 export GOARCH=amd64
-go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$TAG-$GOOS-$GOARCH"
+go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$VERSION-$GOOS-$GOARCH"
 
 export GOOS=linux
 export GOARCH=amd64
-go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$TAG-$GOOS-$GOARCH"
+go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$VERSION-$GOOS-$GOARCH"
 
 export GOARCH=arm
-go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$TAG-$GOOS-$GOARCH"
+go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$VERSION-$GOOS-$GOARCH"
 
 export GOARCH=arm64
-go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$TAG-$GOOS-$GOARCH"
+go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$VERSION-$GOOS-$GOARCH"
 
 export GOARCH=mips GOMIPS=softfloat
-go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$TAG-$GOOS-$GOARCH-$GOMIPS"
+go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$VERSION-$GOOS-$GOARCH-$GOMIPS"
 
 export GOOS=windows
 export GOARCH=amd64
-go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$TAG-$GOOS-$GOARCH"
+go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$VERSION-$GOOS-$GOARCH"
