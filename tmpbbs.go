467f4aebd9f85ed4b835e6e959c34fcf3893f780
Matthew Boedicker <24275+mmb@users.noreply.github.com>
2025-03-02 10:38:19 -0800

    Create viper instance and move setup out of init()

--- tmpbbs.go
+++ tmpbbs.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/mmb/tmpbbs/internal/tmpbbs"
 	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
 )
 
 var version = "unspecified"
@@ -20,39 +19,12 @@ var version = "unspecified"
 //go:embed static
 var staticFS embed.FS
 
-func init() {
-	pflag.StringP("listen-address", "l", ":8080", "<host>:port to listen on ($TMPBBS_LISTEN_ADDRESS)")
-	pflag.StringP("tls-cert", "c", "", "path to PEM server certificate ($TMPBBS_TLS_CERT)")
-	pflag.StringP("tls-key", "k", "", "path to PEM server key ($TMPBBS_TLS_KEY)")
-	pflag.StringP("title", "t", "tmpbbs", "site title ($TMPBBS_TITLE)")
-	pflag.StringP("trip-code-salt", "a", "", "random salt to use for generating trip codes ($TMPBBS_TRIP_CODE_SALT)")
-	pflag.StringSliceP("load-posts", "p", []string{},
-		`comma-separated paths of YAML or JSON files of posts to load, format [{"title":"","author":"","body":""}]
-($TMPBBS_LOAD_POSTS)`)
-	pflag.StringSliceP("serve-fs-paths", "f", []string{},
-		"comma-separated list of urlprefix=/local/dir to serve ($TMPBBS_SERVE_FS_PATHS)")
-	pflag.IntP("replies-per-page", "e", 10, "number of replies to show per page ($TMPBBS_REPLIES_PER_PAGE)")
-	pflag.StringSliceP("css-urls", "u", []string{"/static/main.css"},
-		"comma-separated list of CSS URLs ($TMPBBS_CSS_URLS)")
-	pflag.BoolP("replies", "r", true, "enable replies ($TMPBBS_REPLIES)")
-	pflag.BoolP("emoji", "m", true, "enable emoji shortcode expansion ($TMPBBS_EMOJI)")
-	pflag.BoolP("qr-codes", "q", true, "enable shareable URL QR codes ($TMPBBS_QR_CODES)")
-	pflag.BoolP("version", "v", false, "print version")
-	pflag.BoolP("help", "h", false, "usage help")
-
-	pflag.Parse()
-
-	err := viper.BindPFlags(pflag.CommandLine)
+func main() {
+	viper, err := tmpbbs.NewViper()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	viper.AutomaticEnv()
-	viper.SetEnvPrefix("tmpbbs")
-	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
-}
-
-func main() {
 	if viper.GetBool("help") {
 		pflag.CommandLine.SortFlags = false
 		pflag.Usage()
