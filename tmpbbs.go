e3a0105a93e62083e1bbacdf1d1febabb5fabaf4
Matthew Boedicker <24275+mmb@users.noreply.github.com>
2025-03-01 20:54:10 -0800

    Fix lll violations

--- tmpbbs.go
+++ tmpbbs.go
@@ -26,10 +26,14 @@ func init() {
 	pflag.StringP("tls-key", "k", "", "path to PEM server key ($TMPBBS_TLS_KEY)")
 	pflag.StringP("title", "t", "tmpbbs", "site title ($TMPBBS_TITLE)")
 	pflag.StringP("trip-code-salt", "a", "", "random salt to use for generating trip codes ($TMPBBS_TRIP_CODE_SALT)")
-	pflag.StringSliceP("load-posts", "p", []string{}, `comma-separated paths of YAML or JSON files of posts to load, format [{"title":"","author":"","body":""}] ($TMPBBS_LOAD_POSTS)`)
-	pflag.StringSliceP("serve-fs-paths", "f", []string{}, "comma-separated list of urlprefix=/local/dir to serve ($TMPBBS_SERVE_FS_PATHS)")
+	pflag.StringSliceP("load-posts", "p", []string{},
+		`comma-separated paths of YAML or JSON files of posts to load, format [{"title":"","author":"","body":""}]
+($TMPBBS_LOAD_POSTS)`)
+	pflag.StringSliceP("serve-fs-paths", "f", []string{},
+		"comma-separated list of urlprefix=/local/dir to serve ($TMPBBS_SERVE_FS_PATHS)")
 	pflag.IntP("replies-per-page", "e", 10, "number of replies to show per page ($TMPBBS_REPLIES_PER_PAGE)")
-	pflag.StringSliceP("css-urls", "u", []string{"/static/main.css"}, "comma-separated list of CSS URLs ($TMPBBS_CSS_URLS)")
+	pflag.StringSliceP("css-urls", "u", []string{"/static/main.css"},
+		"comma-separated list of CSS URLs ($TMPBBS_CSS_URLS)")
 	pflag.BoolP("replies", "r", true, "enable replies ($TMPBBS_REPLIES)")
 	pflag.BoolP("emoji", "m", true, "enable emoji shortcode expansion ($TMPBBS_EMOJI)")
 	pflag.BoolP("qr-codes", "q", true, "enable shareable URL QR codes ($TMPBBS_QR_CODES)")
@@ -88,7 +92,8 @@ func main() {
 		http.Handle("GET /qr", tmpbbs.NewQRCodeGetHandler())
 	}
 
-	postGetHandler := tmpbbs.NewPostGetHandler(repliesPerPage, viper.GetStringSlice("css-urls"), repliesEnabled, viper.GetBool("emoji"), qrCodesEnabled, postStore)
+	postGetHandler := tmpbbs.NewPostGetHandler(repliesPerPage, viper.GetStringSlice("css-urls"), repliesEnabled,
+		viper.GetBool("emoji"), qrCodesEnabled, postStore)
 	http.Handle("GET /{$}", postGetHandler)
 	http.Handle("GET /{id}", postGetHandler)
 
