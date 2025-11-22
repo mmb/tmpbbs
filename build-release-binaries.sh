977161f60286d0912818311caa9288a8adeee433
Matthew Boedicker <24275+mmb@users.noreply.github.com>
2025-11-21 17:20:42 -0800

    Always disable cgo

--- build-release-binaries.sh
+++ build-release-binaries.sh
@@ -8,23 +8,25 @@ COMMIT=$2
 export RELEASE_DIR=release
 mkdir -p "$RELEASE_DIR"
 
+export CGO_ENABLED=0
+
 BUILD_ARGS=(-ldflags "-s -w -X github.com/mmb/tmpbbs/internal/tmpbbs.Version=$VERSION -X github.com/mmb/tmpbbs/internal/tmpbbs.Commit=$COMMIT")
 
 build() {
-  export GOOS=$1 GOARCH=$2 CGO_ENABLED=$3
+  export GOOS=$1 GOARCH=$2
 
   go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$VERSION-$GOOS-$GOARCH"
 }
 
-build android arm64 0
-build darwin amd64 0
-build darwin arm64 0
-build linux 386 0
-build linux amd64 0
-build linux arm 0
-build linux arm64 0
-build windows 386 0
-build windows amd64 0
+build android arm64
+build darwin amd64
+build darwin arm64
+build linux 386
+build linux amd64
+build linux arm
+build linux arm64
+build windows 386
+build windows amd64
 
-export GOOS=linux GOARCH=mips GOMIPS=softfloat CGO_ENABLED=0
+export GOOS=linux GOARCH=mips GOMIPS=softfloat
 go build "${BUILD_ARGS[@]}" -o "$RELEASE_DIR/tmpbbs-$VERSION-$GOOS-$GOARCH-$GOMIPS"
