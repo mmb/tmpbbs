e11c6ca54e433319c2f94381652f6e93ba7f6185
Matthew Boedicker <24275+mmb@users.noreply.github.com>
2026-03-03 21:50:09 -0800

    Shared integration test setup

--- tests/integration/integration_suite_test.go
+++ tests/integration/integration_suite_test.go
@@ -3,6 +3,7 @@ package integration_test
 import (
 	"fmt"
 	"os/exec"
+	"path/filepath"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -11,50 +12,45 @@ import (
 	"github.com/onsi/gomega/gexec"
 )
 
-const (
-	mainPort      = 7800
-	mainOverlay   = "kustomize/main"
-	mainNamespace = "tmpbbs"
-)
+const mainPort = 7800
 
-var (
-	mainURL                = fmt.Sprintf("http://localhost:%d", mainPort)
-	mainPortForwardSession *gexec.Session
-)
+var mainURL = fmt.Sprintf("http://localhost:%d", mainPort)
 
 var _ = BeforeSuite(func() {
-	var (
-		session *gexec.Session
-		err     error
-	)
-
-	command := exec.Command("kubectl", "apply", "--kustomize", mainOverlay)
-	session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
-	Expect(err).NotTo(HaveOccurred())
-	Eventually(session).Should(gexec.Exit(0))
-	DeferCleanup(func() {
-		deleteCommand := exec.Command("kubectl", "delete", "--kustomize", mainOverlay)
-		deleteSession, deleteErr := gexec.Start(deleteCommand, GinkgoWriter, GinkgoWriter)
-		Expect(deleteErr).NotTo(HaveOccurred())
-		Eventually(deleteSession, "30s").Should(gexec.Exit(0))
-	})
-
-	command = exec.Command("kubectl", "rollout", "status", "statefulset/tmpbbs", "--namespace", mainNamespace)
-	session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
-	Expect(err).NotTo(HaveOccurred())
-	Eventually(session, "2m30s").Should(gexec.Exit(0))
-
-	command = exec.Command("kubectl", "port-forward", "service/tmpbbs-http", "--namespace", mainNamespace,
-		fmt.Sprintf("%d:8080", mainPort))
-	mainPortForwardSession, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
-	Expect(err).NotTo(HaveOccurred())
-	Eventually(mainPortForwardSession, "10s").Should(gbytes.Say("Forwarding from"))
-	DeferCleanup(func() {
-		mainPortForwardSession.Terminate()
-	})
+	deployOverlay("main", mainPort)
 })
 
 func TestIntegration(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Integration Suite")
+}
+
+func deployOverlay(name string, port int) {
+	path := filepath.Join("kustomize", name)
+	namespace := "tmpbbs-" + name
+
+	command := exec.Command("kubectl", "apply", "--kustomize", path)
+	session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session).Should(gexec.Exit(0))
+	DeferCleanup(func() {
+		deleteCommand := exec.Command("kubectl", "delete", "--kustomize", path)
+		deleteSession, deleteErr := gexec.Start(deleteCommand, GinkgoWriter, GinkgoWriter)
+		Expect(deleteErr).NotTo(HaveOccurred())
+		Eventually(deleteSession, "30s").Should(gexec.Exit(0))
+	})
+
+	command = exec.Command("kubectl", "rollout", "status", "statefulset/tmpbbs", "--namespace", namespace)
+	session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session, "2m30s").Should(gexec.Exit(0))
+
+	command = exec.Command("kubectl", "port-forward", "service/tmpbbs-http", "--namespace", namespace,
+		fmt.Sprintf("%d:8080", port))
+	portForwardSession, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(portForwardSession, "10s").Should(gbytes.Say("Forwarding from"))
+	DeferCleanup(func() {
+		portForwardSession.Terminate()
+	})
 }
--- tests/integration/peer_test.go
+++ tests/integration/peer_test.go
@@ -5,48 +5,18 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"os/exec"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gbytes"
-	"github.com/onsi/gomega/gexec"
-)
-
-const (
-	peerPort      = 7801
-	peerOverlay   = "kustomize/peer"
-	peerNamespace = "tmpbbs-peer"
 )
 
 var _ = Describe("peer", Ordered, func() {
-	peerURL := fmt.Sprintf("http://localhost:%d", peerPort)
+	var tmpbbsURL string
 
 	BeforeAll(func() {
-		command := exec.Command("kubectl", "apply", "--kustomize", peerOverlay)
-		session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-		Expect(err).NotTo(HaveOccurred())
-		Eventually(session).Should(gexec.Exit(0))
-		DeferCleanup(func() {
-			deleteCommand := exec.Command("kubectl", "delete", "--kustomize", peerOverlay)
-			deleteSession, deleteErr := gexec.Start(deleteCommand, GinkgoWriter, GinkgoWriter)
-			Expect(deleteErr).NotTo(HaveOccurred())
-			Eventually(deleteSession, "30s").Should(gexec.Exit(0))
-		})
-
-		command = exec.Command("kubectl", "rollout", "status", "statefulset/tmpbbs", "--namespace", peerNamespace)
-		session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
-		Expect(err).NotTo(HaveOccurred())
-		Eventually(session, "1m").Should(gexec.Exit(0))
-
-		command = exec.Command("kubectl", "port-forward", "service/tmpbbs-http", "--namespace", peerNamespace,
-			fmt.Sprintf("%d:8080", peerPort))
-		peerPortForwardSession, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-		Expect(err).NotTo(HaveOccurred())
-		Eventually(peerPortForwardSession, "10s").Should(gbytes.Say("Forwarding from"))
-		DeferCleanup(func() {
-			peerPortForwardSession.Terminate()
-		})
+		port := 7801
+		deployOverlay("peer", port)
+		tmpbbsURL = fmt.Sprintf("http://localhost:%d", port)
 	})
 
 	It("pulls a post from main", func() {
@@ -59,7 +29,7 @@ var _ = Describe("peer", Ordered, func() {
 		Expect(resp.StatusCode).To(Equal(http.StatusOK))
 
 		Eventually(func() string {
-			peerResp, peerErr := http.Get(peerURL)
+			peerResp, peerErr := http.Get(tmpbbsURL)
 			Expect(peerErr).NotTo(HaveOccurred())
 			Expect(peerResp.StatusCode).To(Equal(http.StatusOK))
 			body, bodyErr := io.ReadAll(peerResp.Body)
